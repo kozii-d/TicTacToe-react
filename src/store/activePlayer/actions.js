94d9bbdea1ad35357064b98a945d5740e11ee29c
Dmitrii Kozii <dkozii1807@gmail.com>
2022-04-22 00:39:15 +0300

    delete comma

--- src/store/activePlayer/actions.js
+++ src/store/activePlayer/actions.js
@@ -1,4 +1,4 @@
 import {createAction} from "redux-actions";
 
-export const toggleActivePlayer = createAction('TOGGLE_ACTIVE_PLAYER', );
+export const toggleActivePlayer = createAction('TOGGLE_ACTIVE_PLAYER');
 export const restart = createAction('RESTART');
